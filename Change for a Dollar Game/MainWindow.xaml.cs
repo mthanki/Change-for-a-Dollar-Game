6f73a3acb967a7ca694aa82a9857ae9b5722cdfc
mthanki <60077334+mthanki@users.noreply.github.com>
2021-02-17 01:59:37 -0500

    Add project files.

---
+++ Change for a Dollar Game/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Change_for_a_Dollar_Game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        VM vm = new VM();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = vm;
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            TextBox tb = sender as TextBox;
+            e.Handled = !int.TryParse(tb.Text + e.Text, out _);
+        }
+
+        private void Coin_Click(object sender, RoutedEventArgs e)
+        {
+            var Button = sender as Button;
+            var prop = Button.Tag;
+
+            vm.IncrementCoinCount(prop);
+        }
+
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            vm.CalculateResult();
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            vm.ResetGame();
+        }
+    }
+}
---
+++ Change for a Dollar Game/VM.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Change_for_a_Dollar_Game
+{
+    enum Coins
+    {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter
+    }
+
+    class VM : INotifyPropertyChanged
+    {
+        const int PENNY = 1;
+        const int NICKEL = 5;
+        const int DIME = 10;
+        const int QUARTER = 25;
+
+        const string START_UP_MESSAGE = "Pick a number of Coins & Hit GO to start playing!";
+        const string WIN_MESSAGE = "WINNER, that makes a Dollar!";
+
+        private int penny = 0;
+        public int Penny {
+            get { return penny; }
+            set { penny = value; propertyChanged(); } 
+        }
+
+        private int nickel = 0;
+        public int Nickel {
+            get { return nickel; }
+            set { nickel = value; propertyChanged(); } 
+        }
+
+        private int dime = 0;
+        public int Dime { 
+            get { return dime; }
+            set { dime = value; propertyChanged(); } 
+        }
+
+        private int quarter = 0;
+        public int Quarter { 
+            get { return quarter; }
+            set { quarter = value; propertyChanged(); } 
+        }
+
+        private string result = START_UP_MESSAGE;
+        public string Result { 
+            get { return result; } 
+            set { result = value; propertyChanged(); }
+        }
+
+        private string resetButtonVisibility = "Collapsed";
+        public string ResetButtonVisibility
+        {
+            get { return resetButtonVisibility; }
+            set { resetButtonVisibility = value; propertyChanged(); }
+        }
+
+        public void CalculateResult()
+        {
+            int Result;
+            int TotalCents = (Penny * PENNY) + (Nickel * NICKEL) + (Dime * DIME) + (QUARTER * Quarter);
+            Result = 100 - TotalCents;
+            UpdateResultLabel(Result);
+        }
+
+        public string GetResultMoreMessage(int R)
+        {
+            return $"OOPS! That's {Math.Abs(R):N} cents more than a Dollar";
+        }
+
+        public string GetResultLessMessage(int R)
+        {
+            return $"OOPS! That's {R} cents short of a Dollar";
+        }
+
+        public string GetWinMessage()
+        {
+            return WIN_MESSAGE;
+        }
+
+        public void UpdateResultLabel(int Res)
+        {
+            if (Res == 0)
+            {
+                Result = GetWinMessage();
+            }
+            else if (Res < 0)
+            {
+                Result = GetResultMoreMessage(Res);
+            }
+            else
+            {
+                Result = GetResultLessMessage(Res);
+            }
+
+            ResetButtonVisibility = "Visible";
+        }
+
+        public void ResetGame()
+        {
+            Penny = Nickel = Dime = Quarter = 0;
+            ResetButtonVisibility = "Collapsed";
+            Result = START_UP_MESSAGE;
+        }
+
+        public void IncrementCoinCount(object prop)
+        {
+            var CoinSelected = prop;
+
+            switch (prop)
+            {
+                case "Penny":
+                    Penny++;
+                    break;
+                case "Nickel":
+                    Nickel++;
+                    break;
+                case "Dime":
+                    Dime++;
+                    break;
+                case "Quarter":
+                    Quarter++;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #region Property Changed
+        private void propertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
